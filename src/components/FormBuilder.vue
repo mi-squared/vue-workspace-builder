e831fa4fbe9504815884ca42fe4d1b8e74388599
Jacob Coby <jacob@mi-squared.com>
2021-08-16 11:45:12 -0400

    Switch to vue-grid-layout

--- src/components/FormBuilder.vue
+++ src/components/FormBuilder.vue
@@ -1,65 +1,72 @@
 <template>
-<div id="layout-builder">
-  <v-container>
-    <v-row>
-      <v-col
-          cols="12"
-          sm="8"
-      >
-        <v-sheet
-            rounded="lg"
-            min-height="268"
-        >
-          <vue-grid-designer v-model="grid" @add-block="onBlockAdded" ></vue-grid-designer>
+  <div id="layout-builder">
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="8">
+          <v-btn @click="addItem">Add Form Element</v-btn>
+          <v-sheet rounded="lg" min-height="268" elevation="1">
+            <grid-layout
+              :layout.sync="grid"
+              :col-num="12"
+              :row-height="30"
+              :is-draggable="true"
+              :is-resizable="true"
+              :is-mirrored="false"
+              :vertical-compact="true"
+              :margin="[10, 10]"
+              :use-css-transforms="true"
+            >
+              <grid-item
+                v-for="item in grid"
+                :x="item.x"
+                :y="item.y"
+                :w="item.w"
+                :h="item.h"
+                :i="item.i"
+                :key="item.i"
+              >
+                {{ item.i }}
+              </grid-item>
+            </grid-layout>
+          </v-sheet>
+        </v-col>
 
-        </v-sheet>
-      </v-col>
+        <v-col cols="12" sm="4">
+          <FormProperties :form-model="activeFormModel"></FormProperties>
 
-      <v-col
-          cols="12"
-          sm="4"
-      >
-        <FormProperties :form-model="activeFormModel"></FormProperties>
+          <v-card rounded="lg" min-height="268" class="mt-0 p-4">
+            <v-card-title>Element Properties</v-card-title>
+            <v-card-text> </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 
-        <v-card
-            rounded="lg"
-            min-height="268"
-            class="mt-0 p-4"
-        >
-
-          <v-card-title>Element Properties</v-card-title>
-          <v-card-text>
-
-          </v-card-text>
-        </v-card>
-
-      </v-col>
-    </v-row>
-  </v-container>
-
-  <v-dialog v-model="showFormElementSelector">
-    <FormElementSelector></FormElementSelector>
-  </v-dialog>
-</div>
+    <v-dialog v-model="showFormElementSelector">
+      <FormElementSelector></FormElementSelector>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-import VueGridDesigner from '@thefoot/vue-grid-designer'
-import FormElementSelector from "@/components/FormElementSelector"
-import FormProperties from '@/components/FormProperties'
+import VueGridLayout from "vue-grid-layout";
+import FormElementSelector from "@/components/FormElementSelector";
+import FormProperties from "@/components/FormProperties";
 
 export default {
-  name: 'LayoutBuilder',
+  name: "LayoutBuilder",
   components: {
     FormProperties,
     FormElementSelector,
-    VueGridDesigner
+    // VueGridDesigner,
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem,
   },
   props: {
     form: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   data: function() {
     return {
@@ -68,24 +75,39 @@ export default {
       layouts: [
         {
           id: 1,
-          title: 'Main Form'
-        }
+          title: "Main Form",
+        },
       ],
       grid: [],
-    }
+      index: 0,
+    };
   },
   computed: {
-    activeFormModel () {
-      return this.layouts[this.activeLayoutIndex]
+    activeFormModel() {
+      return this.layouts[this.activeLayoutIndex];
     },
   },
+  mounted() {
+    this.index = this.layout.length;
+  },
   methods: {
-    onBlockAdded () {
-      this.showFormElementSelector = true
-      this.$refs.foo.show('example-modal')
-    }
-  }
-}
+    addItem: function() {
+      this.showFormElementSelector = true;
+      // this.$refs.foo.show("example-modal");
+
+      // Add a new item. It must have a unique key!
+      this.grid.push({
+        x: (this.grid.length * 2) % (this.colNum || 12),
+        y: this.grid.length + (this.colNum || 12), // puts it at the bottom
+        w: 12,
+        h: 2,
+        i: this.index,
+      });
+      // Increment the counter to ensure key is always unique.
+      this.index++;
+    },
+  },
+};
 </script>
 
 <style>
