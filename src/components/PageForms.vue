13ffc1b7e9cde34643c70ad6a2bcc562d7b5285d
Jacob Coby <jacob@mi-squared.com>
2021-08-19 15:57:32 -0400

    Get rid of v-row, use the url params to set workspace in the form builder

--- src/components/PageForms.vue
+++ src/components/PageForms.vue
@@ -1,61 +1,59 @@
 <template>
   <v-container>
-    <v-row align="start">
-      <!-- This is the dialog at the top for creating a new layout -->
-      <v-tabs vertical>
-        <v-dialog v-model="showNewFormDialog" persistent max-width="600px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn text class="align-self-center mr-4" v-bind="attrs" v-on="on">
-              <v-icon left>
-                mdi-plus
-              </v-icon>
-              New Form
+    <!-- This is the dialog at the top for creating a new layout -->
+    <v-tabs vertical>
+      <v-dialog v-model="showNewFormDialog" persistent max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text class="align-self-center mr-4" v-bind="attrs" v-on="on">
+            <v-icon left>
+              mdi-plus
+            </v-icon>
+            New Form
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">New Form</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-text-field
+                v-model="newFormModel.title"
+                label="Form Title*"
+                required
+              ></v-text-field>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="showNewFormDialog = false"
+            >
+              Close
             </v-btn>
-          </template>
+            <v-btn color="blue darken-1" text @click="saveNewForm">
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- End of the new layout dialog -->
 
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">New Form</span>
-            </v-card-title>
-            <v-card-text>
-              <v-container>
-                <v-text-field
-                  v-model="newFormModel.title"
-                  label="Form Title*"
-                  required
-                ></v-text-field>
-              </v-container>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="showNewFormDialog = false"
-              >
-                Close
-              </v-btn>
-              <v-btn color="blue darken-1" text @click="saveNewForm">
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <!-- End of the new layout dialog -->
+      <!-- this is the list of existing layouts -->
+      <v-tab v-for="form in forms" :key="form.id">
+        {{ form.title }}
+      </v-tab>
+      <!-- end of existing forms -->
 
-        <!-- this is the list of existing layouts -->
-        <v-tab v-for="form in forms" :key="form.id">
-          {{ form.title }}
-        </v-tab>
-        <!-- end of existing forms -->
-
-        <v-tab-item v-for="form in forms" :key="form.id">
-          <v-card flat>
-            <FormBuilder :form="form"></FormBuilder>
-          </v-card>
-        </v-tab-item>
-      </v-tabs>
-    </v-row>
+      <v-tab-item v-for="form in forms" :key="form.id">
+        <v-card flat>
+          <FormBuilder :form="form"></FormBuilder>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
   </v-container>
 </template>
 
@@ -77,9 +75,10 @@ export default {
   },
   computed: {
     activeWorkspace() {
-      return this.$store.state.workspaces[
-        this.$store.state.userState.activeWorkspace
-      ];
+      return this.$store.state.workspaces[this.$route.params.workspaceId];
+    },
+    activeForm() {
+      return this.activeWorspace.forms[this.$route.params.formId];
     },
     forms() {
       return this.activeWorkspace.forms;
