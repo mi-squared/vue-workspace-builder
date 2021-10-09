8f0190fb2725a1b67056188969f6b509a06177e5
Ken Chapple <ken@mi-squared.com>
2021-10-08 20:27:28 -0400

    added dashboardrows response

---
+++ src/data/response.dashboardrows_id_6.js
@@ -0,0 +1,91 @@
+export const dashboardrows_id_6 = [{
+  'id': '13',
+  'moved_to_dashboard_date': '2021-10-05 16:42:21',
+  'DOB': null,
+  'pid': null,
+  'city': null,
+  'fname': null,
+  'lname': null,
+  'something': 'dsafdsfad',
+  'source': 'fadsf'
+}, {
+  'id': '12',
+  'moved_to_dashboard_date': '2021-10-05 16:30:27',
+  'DOB': null,
+  'pid': null,
+  'city': null,
+  'fname': null,
+  'lname': null,
+  'something': 'test',
+  'source': 'test'
+}, {
+  'id': '11',
+  'moved_to_dashboard_date': '2021-10-05 16:19:56',
+  'DOB': null,
+  'pid': null,
+  'city': null,
+  'fname': null,
+  'lname': null,
+  'something': 'test',
+  'source': 'test'
+}, {
+  'id': '10',
+  'moved_to_dashboard_date': '2021-10-01 16:05:23',
+  'DOB': null,
+  'pid': null,
+  'city': null,
+  'fname': null,
+  'lname': null,
+  'something': 'tests',
+  'source': 'test'
+}, {
+  'id': '9',
+  'moved_to_dashboard_date': '2021-10-01 15:59:06',
+  'DOB': null,
+  'pid': null,
+  'city': null,
+  'fname': null,
+  'lname': null,
+  'something': 'test',
+  'source': 'test'
+}, {
+  'id': '8',
+  'moved_to_dashboard_date': '2021-10-01 15:37:31',
+  'DOB': null,
+  'pid': null,
+  'city': null,
+  'fname': null,
+  'lname': null,
+  'something': 'test',
+  'source': 'test'
+}, {
+  'id': '7',
+  'moved_to_dashboard_date': '2021-10-01 15:30:56',
+  'DOB': null,
+  'pid': null,
+  'city': null,
+  'fname': null,
+  'lname': null,
+  'something': 'something',
+  'source': 'test'
+}, {
+  'id': '5',
+  'moved_to_dashboard_date': '2021-10-01 11:40:21',
+  'DOB': '1933-01-01',
+  'pid': '21',
+  'city': '',
+  'fname': 'JIMMY',
+  'lname': 'SMITH',
+  'something': 'something',
+  'source': 'source'
+}, {
+  'id': '1',
+  'moved_to_dashboard_date': '2021-09-30 20:34:36',
+  'DOB': '1988-03-22',
+  'pid': '22',
+  'city': '',
+  'fname': 'JUDY',
+  'lname': 'JETSON',
+  'something': 'test something',
+  'source': 'test'
+}]
