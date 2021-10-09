48ce60a8fbadc42899abe1d67f2dc9a6dd2b5426
Ken Chapple <ken@mi-squared.com>
2021-10-08 20:29:29 -0400

    added post entity response

---
+++ src/data/response.post.entity.js
@@ -0,0 +1,13 @@
+export const entity = {
+  'source': 'test',
+  'something': 'test',
+  'insurance': 'York Risk Services Group',
+  'sex': 'Other',
+  'dashboard_id': 6,
+  'created_datetime': '2021-10-08 20:27:53',
+  'created_by': '1',
+  'updated_datetime': '2021-10-08 20:27:53',
+  'updated_by': '1',
+  'moved_to_dashboard_date': '2021-10-08 20:27:53',
+  'moved_to_dashboard_by': '1'
+}
