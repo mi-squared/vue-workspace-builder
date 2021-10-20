07b0bcd44825ed84044ab9fa8f3548e5c488aecd
Ken Chapple <ken@mi-squared.com>
2021-10-20 11:33:01 -0400

    added new response format for paginated/searchable entity request

---
+++ src/data/request.dashboardrows_id_6.js
@@ -0,0 +1,17 @@
+export const request_dashboardrows_id_6 = {
+  uri: "apis/api/entities",
+  dashboardId : 6,
+  dashboardFilterEnabled : true,
+  archivedFilterEnabled : false,
+  search : '',
+  paginationOptions : {
+    "page": 1,
+    "itemsPerPage": 10,
+    "sortBy": ["moved_to_dashboard_date"],
+    "sortDesc": [true],
+    "groupBy": [],
+    "groupDesc": [],
+    "mustSort": false,
+    "multiSort": false
+  }
+}
