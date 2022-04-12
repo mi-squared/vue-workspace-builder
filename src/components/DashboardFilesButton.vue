f0911366344e9b52626fffe3da723c5a37978d36
Ken Chapple <ken@mi-squared.com>
2022-04-12 14:29:00 -0400

    intermediate

--- src/components/DashboardFilesButton.vue
+++ src/components/DashboardFilesButton.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <v-row justify="center">
     <v-menu
-      :close-on-click="true"
+      :close-on-click="closeOnClick"
     >
       <template v-slot:activator="{ on, attrs }">
         <v-btn
@@ -12,7 +12,7 @@
           <v-badge
             :content="fileCount"
             :value="fileCount"
-            color="orange"
+            :color="orange"
             overlap
           >
             <v-icon>
@@ -26,14 +26,64 @@
         <v-list-item
           v-for="(item, index) in files"
           :key="index"
-          @click="onClick(item)"
         >
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-list-item-title>
+            <a @click="onClickView(item)">{{ item.title }}</a>
+          </v-list-item-title>
+
+          <v-list-item-icon>
+            <v-btn icon @click="onClickView(item)">
+              <v-icon>mdi-eye-arrow-right-outline</v-icon>
+            </v-btn>
+          </v-list-item-icon>
+
+          <v-list-item-icon>
+            <v-btn icon @click.stop="onClickApprove(item)">
+              <v-icon>mdi-check-circle-outline</v-icon>
+            </v-btn>
+          </v-list-item-icon>
+
+          <v-list-item-icon>
+            <v-btn icon @click.stop="onClickDelete(item)">
+              <v-icon>mdi-close-circle-outline</v-icon>
+            </v-btn>
+          </v-list-item-icon>
         </v-list-item>
       </v-list>
 
     </v-menu>
-  </div>
+
+    <!-- Confirmation dialog -->
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          {{ confirmTitle }}
+        </v-card-title>
+        <v-card-text>{{ confirmText }}</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="gray darken-1"
+            text
+            @click="onClickConfirm()"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            :color="confirmColor"
+            text
+            @click="onClickConfirm()"
+          >
+            Confirm
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
 
 <script>
@@ -51,7 +101,13 @@ export default {
   },
   data () {
     return {
-      menu: false
+      menu: false,
+      action: '',
+      activeItem: null,
+      dialog: false,
+      confirmTitle: '',
+      confirmText: '',
+      confirmColor: ''
     }
   },
   computed: {
@@ -65,21 +121,58 @@ export default {
       } else {
         return []
       }
+    },
+    closeOnClick() {
+      // Don't close unless there is no confirm dialog open
+      if (this.dialog == false) {
+        return true
+      }
+      return false
     }
   },
   methods: {
-    onClick (item) {
+    onClickView (item) {
       // If there is one file, then open the file, if there is more than one, open selector
       const pdf = '/openemr/controller.php?document&retrieve&patient_id=' +
         item.pid +
         '&document_id=' +
         item.metaValue;
       window.open(pdf);
+    },
+    onClickConfirm()
+    {
+      this.dialog = false
+    },
+    onClickApprove (item)
+    {
+      // TODO implement approve file and send to NS
+      this.activeItem = item
+      this.action = 'approve'
+      this.confirmText = "I have reviewed the file and ready to transmit to Next Step"
+      this.confirmTitle = "Confirm Approval"
+      this.confirmColor = "green darken-1"
+      this.dialog = true
+      return item
+    },
+    onClickDelete (item)
+    {
+      // TODO implement approve file and remove from Document tree
+      this.activeItem = item
+      this.action = 'delete'
+      this.confirmText = "I have reviewed the file and confirmed it is invalid and wish to delete"
+      this.confirmTitle = "Confirm Delete"
+      this.confirmColor = "red darken-1"
+      this.dialog = true
+      return item
     }
   }
 }
 </script>
 
 <style scoped>
+
+.v-list-item__icon {
+  margin-left: 16px;
+}
 
 </style>
